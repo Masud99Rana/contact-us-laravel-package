251766cff6eeaa1c12bef524a9b7bc00677635f2
Masud99Rana <Masudrana917775@gmail.com>
2020-01-09 20:38:19 +0600

    Initial Commit, composer & autoload setup

---
+++ src/ContactServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Masud99Rana\Contact;
+use Illuminate\Support\ServiceProvider;
+
+class ContactServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        
+    }
+    public function register()
+    {
+    }
+}
